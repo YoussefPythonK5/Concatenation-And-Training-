a3260ed9dd4d28e1e9f548b3f36fe2724e60f855
Youssef <ya103857@gmail.com>
2026-07-17 23:52:00 +0300

    Add files via upload

---
+++ Concatenation.py
@@ -0,0 +1,28 @@
+
+# Concatenation
+msg = "I love"
+land = "Python"
+
+print (msg + " " + land)
+
+full = msg + " "  + land
+print(full)
+
+
+a = "First \
+Second \
+Third"
+print (a)
+
+b = "A \
+B \
+C"
+print (a +" \n" + b) # عشان اعمل سطر جديد اضيف \n بين علامات الاقتباس او علامات التنصيص
+
+print ("Hello" + 1) # Error
+
+# مينفعش نكتب نص (String) مع رقم (Integer) بس نقدر نحول الرقم لنص بإستخدام علامات الاقتباس
+
+print ("Hello" + " " + "1")
+
+
